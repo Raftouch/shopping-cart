1c35ae6a00c85e5ce88a7d87d01b0aaa3832c5db
Raftouch <rafanelkin@gmail.com>
2023-02-26 20:58:48 +0100

    :sparkles: added new components, new hook

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,11 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import iconCart from "../images/iconCart.png"
 
 export function Navigation() {
+    const {openCart, cartQuantity} = useShoppingCart()
+
     return (
         <Navbar className="bg-white shadow-sm mb-3" sticky="top">
             <Container>
@@ -17,18 +20,24 @@ export function Navigation() {
                         Store
                     </Nav.Link>
                 </Nav>
-                <Button
-                    style={{ width: "3.5rem", height: "3.5rem", position: "relative" }}
-                    variant="outline-primary"
-                    className="rounded-circle"
-                    >
-                    <img style={{width: "2rem"}} src={iconCart} />
-                    <div 
-                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-                        style={{color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"}}>
-                        4
-                    </div>
-                </Button>
+                {cartQuantity > 0 && (
+                    <Button
+                        onClick={openCart}
+                        style={{ width: "3.5rem", height: "3.5rem", position: "relative" }}
+                        variant="outline-primary"
+                        className="rounded-circle"
+                        >
+                        <img style={{width: "2rem"}} src={iconCart} />
+                        <div 
+                            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                            style={{
+                                color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)"
+                            }}
+                            >
+                            {cartQuantity}
+                        </div>
+                    </Button>
+                )}
             </Container>
         </Navbar>
     )
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,10 +1,16 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 interface ShoppingCartContext {
+    openCart: () => void
+    closeCart: () => void
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[]
 }
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext)
@@ -23,7 +29,13 @@ interface CartItem {
 }
 
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", [])
+    const [isOpen, setIsOpen] = useState(false)
+
+    const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
+
+    const openCart = () => setIsOpen(true)
+    const closeCart = () => setIsOpen(false)
 
     function getItemQuantity(id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0
@@ -72,10 +84,15 @@ export function ShoppingCartProvider({children}: ShoppingCartProviderProps) {
             getItemQuantity, 
             increaseCartQuantity, 
             decreaseCartQuantity, 
-            removeFromCart
+            removeFromCart,
+            openCart,
+            closeCart,
+            cartItems,
+            cartQuantity
         }}
         >
             {children}
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     )
 }
